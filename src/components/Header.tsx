5d163a325dd8dca9d832892b985b4b18c07596af
John Moore <jmoore@zedstar.org>
2025-02-14 19:56:24 +0000

    experimenting with logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from './SearchBar.tsx';
+import { ReactComponent as Logo } from '../logo.svg'; 
 
 const Header = ({ onSearch }: { onSearch: (query: string) => void }) => {
   return (
-    <header className='bg-gray-800 text-white p-4 flex items-center'>
-      <h1 className='text-xl font-bold mr-4'>IIIF Viewer</h1>
+    <header className='bg-gray-800 text-white p-2 flex items-center'>
+      <Logo className='h-8 w-8 mr-2' alt='Tamerlane IIIF viewer' />
+      <span className='text-lg font-semibold'>Tamerlane</span>
       <div className='ml-auto'>
         <SearchBar onSearch={onSearch} />
       </div>
