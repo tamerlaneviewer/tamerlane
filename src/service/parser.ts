c8f53d59e4dbd967feb0ca3a7fe43b4555bfef1d
John Moore <jmoore@zedstar.org>
2025-03-07 16:44:33 +0000

    start to use prettier

--- src/service/parser.ts
+++ src/service/parser.ts
@@ -3,193 +3,223 @@ import { IIIFManifest, IIIFImage, IIIFCanvas } from '../types/index.ts';
 import { TamerlaneParseError } from '../errors/index.ts';
 import { fetchResource } from './utils.ts';
 
-
 const manifestCache = new Map<string, IIIFManifest>(); // Caching fetched manifests
 
-
-export function getCanvasDimensions(manifest: IIIFManifest, canvasId: string): { canvasWidth: number; canvasHeight: number } {
-    const canvas = manifest.canvases.find(c => c.id === canvasId);
-    if (!canvas) {
-        throw new TamerlaneParseError(`Canvas with ID ${canvasId} not found.`);
-    }
-    return {
-        canvasWidth: canvas.canvasWidth ?? 1000,
-        canvasHeight: canvas.canvasHeight ?? 1000
-    };
+export function getCanvasDimensions(
+  manifest: IIIFManifest,
+  canvasId: string,
+): { canvasWidth: number; canvasHeight: number } {
+  const canvas = manifest.canvases.find((c) => c.id === canvasId);
+  if (!canvas) {
+    throw new TamerlaneParseError(`Canvas with ID ${canvasId} not found.`);
+  }
+  return {
+    canvasWidth: canvas.canvasWidth ?? 1000,
+    canvasHeight: canvas.canvasHeight ?? 1000,
+  };
 }
 
 function getImage(resource: any, canvasTarget: string): IIIFImage {
-    let url: string | undefined;
-    let type: "standard" | "iiif" = "standard";
-    let imageWidth: number | undefined;
-    let imageHeight: number | undefined;
+  let url: string | undefined;
+  let type: 'standard' | 'iiif' = 'standard';
+  let imageWidth: number | undefined;
+  let imageHeight: number | undefined;
 
-    if (resource.service && Array.isArray(resource.service) && resource.service.length > 0) {
-        const service = resource.service[0];
-        url = typeof service.id === 'string' ? service.id
-            : typeof service["@id"] === 'string' ? service["@id"]
-                : undefined;
-        imageWidth = service.width ?? resource.width;
-        imageHeight = service.height ?? resource.height;
-        type = "iiif";
+  if (
+    resource.service &&
+    Array.isArray(resource.service) &&
+    resource.service.length > 0
+  ) {
+    const service = resource.service[0];
+    url =
+      typeof service.id === 'string'
+        ? service.id
+        : typeof service['@id'] === 'string'
+          ? service['@id']
+          : undefined;
+    imageWidth = service.width ?? resource.width;
+    imageHeight = service.height ?? resource.height;
+    type = 'iiif';
+  }
+  if (!url && typeof resource.id === 'string') {
+    url = resource.id;
+    type = 'standard';
+    if (resource.width && resource.height) {
+      imageWidth = resource.width;
+      imageHeight = resource.height;
     }
-    if (!url && typeof resource.id === 'string') {
-        url = resource.id;
-        type = "standard";
-        if (resource.width && resource.height) {
-            imageWidth = resource.width;
-            imageHeight = resource.height;
-        }
-    }
-    if (!url) {
-        throw new TamerlaneParseError("Unable to get image resource id.");
-    }
-    //console.log(`Found image: ${url} with type: ${type} and dimensions: ${imageWidth}x${imageHeight}`);
-    return { imageUrl: url, imageType: type, imageWidth, imageHeight, canvasTarget };
+  }
+  if (!url) {
+    throw new TamerlaneParseError('Unable to get image resource id.');
+  }
+  //console.log(`Found image: ${url} with type: ${type} and dimensions: ${imageWidth}x${imageHeight}`);
+  return {
+    imageUrl: url,
+    imageType: type,
+    imageWidth,
+    imageHeight,
+    canvasTarget,
+  };
 }
 
 async function parseManifest(jsonData: any): Promise<IIIFManifest> {
-    const parser = new Maniiifest(jsonData);
-    const type = parser.getSpecificationType();
-    if (type !== 'Manifest') {
-        throw new TamerlaneParseError('Invalid IIIF resource type: ' + type);
+  const parser = new Maniiifest(jsonData);
+  const type = parser.getSpecificationType();
+  if (type !== 'Manifest') {
+    throw new TamerlaneParseError('Invalid IIIF resource type: ' + type);
+  }
+
+  const labelData: any = parser.getManifestLabelByLanguage('en');
+  const label: string =
+    labelData?.en?.[0] ?? labelData?.none?.[0] ?? 'Untitled Manifest';
+  const metadata = Array.from(parser.iterateManifestMetadata());
+  const provider = Array.from(parser.iterateManifestProvider());
+
+  const canvases: IIIFCanvas[] = [];
+  for (const canvas of parser.iterateManifestCanvas()) {
+    const id = canvas.id;
+    const canvasHeight = canvas.height;
+    const canvasWidth = canvas.width;
+    canvases.push({ id, canvasWidth, canvasHeight });
+  }
+
+  const images: IIIFImage[] = [];
+  for (const anno of parser.iterateManifestCanvasAnnotation()) {
+    const annoParser = new Maniiifest(anno, 'Annotation');
+    const targets = Array.from(annoParser.iterateAnnotationTarget());
+    if (targets.length !== 1) {
+      throw new TamerlaneParseError('Expected a single canvas target');
+    }
+    const canvasTarget = targets[0];
+    if (typeof canvasTarget !== 'string') {
+      throw new TamerlaneParseError('Expected canvas target to be a string');
     }
 
-    const labelData: any = parser.getManifestLabelByLanguage('en');
-    const label: string = labelData?.en?.[0] ?? labelData?.none?.[0] ?? 'Untitled Manifest';
-    const metadata = Array.from(parser.iterateManifestMetadata());
-    const provider = Array.from(parser.iterateManifestProvider());
-    
-    const canvases: IIIFCanvas[] = [];
-    for (const canvas of parser.iterateManifestCanvas()) {
-        const id = canvas.id;
-        const canvasHeight = canvas.height;
-        const canvasWidth = canvas.width;
-        canvases.push({ id, canvasWidth, canvasHeight });
+    for (const resourceBody of annoParser.iterateAnnotationResourceBody()) {
+      const image = getImage(resourceBody, canvasTarget);
+      images.push(image);
     }
+  }
 
-    const images: IIIFImage[] = [];
-    for (const anno of parser.iterateManifestCanvasAnnotation()) {
-        const annoParser = new Maniiifest(anno, "Annotation");
-        const targets = Array.from(annoParser.iterateAnnotationTarget());
-        if (targets.length !== 1) {
-            throw new TamerlaneParseError("Expected a single canvas target");
-        }
-        const canvasTarget = targets[0];
-        if (typeof canvasTarget !== 'string') {
-            throw new TamerlaneParseError("Expected canvas target to be a string");
-        }
-
-        for (const resourceBody of annoParser.iterateAnnotationResourceBody()) {
-            const image = getImage(resourceBody, canvasTarget);
-            images.push(image);
-        }
-    }
-
-    return {
-        name: label,
-        metadata,
-        provider,
-        canvases,
-        images
-    };
+  return {
+    name: label,
+    metadata,
+    provider,
+    canvases,
+    images,
+  };
 }
 
+async function parseCollection(
+  jsonData: any,
+): Promise<{
+  firstManifest: IIIFManifest | null;
+  manifestUrls: string[];
+  total: number;
+}> {
+  const parser = new Maniiifest(jsonData);
 
-async function parseCollection(jsonData: any): Promise<{ firstManifest: IIIFManifest | null, manifestUrls: string[], total: number }> {
-    const parser = new Maniiifest(jsonData);
+  if (parser.getSpecificationType() !== 'Collection') {
+    throw new TamerlaneParseError(
+      'Invalid IIIF resource type: Collection expected',
+    );
+  }
+  const manifestUrls: string[] = [];
+  let firstManifest: IIIFManifest | null = null;
 
-    if (parser.getSpecificationType() !== 'Collection') {
-        throw new TamerlaneParseError('Invalid IIIF resource type: Collection expected');
-    }
-    const manifestUrls: string[] = [];
-    let firstManifest: IIIFManifest | null = null;
+  async function process(parsedJson: any, processedCollections: Set<string>) {
+    if (processedCollections.has(parsedJson.id)) return;
+    processedCollections.add(parsedJson.id);
 
-    async function process(parsedJson: any, processedCollections: Set<string>) {
-        if (processedCollections.has(parsedJson.id)) return;
-        processedCollections.add(parsedJson.id);
+    const parser = new Maniiifest(parsedJson);
+    let foundManifests = false;
 
-        const parser = new Maniiifest(parsedJson);
-        let foundManifests = false;
+    for (const manifestItem of parser.iterateCollectionManifest()) {
+      const manifestRef = new Maniiifest(manifestItem);
+      const manifestId = manifestRef.getManifestId();
 
-        for (const manifestItem of parser.iterateCollectionManifest()) {
-            const manifestRef = new Maniiifest(manifestItem);
-            const manifestId = manifestRef.getManifestId();
+      if (manifestId) {
+        manifestUrls.push(manifestId);
+        foundManifests = true;
 
-            if (manifestId) {
-                manifestUrls.push(manifestId);
-                foundManifests = true;
-
-                if (!firstManifest) {
-                    if (manifestCache.has(manifestId)) {
-                        firstManifest = manifestCache.get(manifestId)!;
-                    } else {
-                        const manifestResource = await fetchResource(manifestId);
-                        firstManifest = await parseManifest(manifestResource.data);
-                        if (firstManifest) {
-                            manifestCache.set(manifestId, firstManifest);
-                        }
-                    }
-                }
+        if (!firstManifest) {
+          if (manifestCache.has(manifestId)) {
+            firstManifest = manifestCache.get(manifestId)!;
+          } else {
+            const manifestResource = await fetchResource(manifestId);
+            firstManifest = await parseManifest(manifestResource.data);
+            if (firstManifest) {
+              manifestCache.set(manifestId, firstManifest);
             }
+          }
         }
-        if (!foundManifests) {
-            for (const collectionItem of parser.iterateCollectionCollection()) {
-                if (collectionItem.items) {
-                    await process(collectionItem, processedCollections);
-                } else {
-                    const nestedJson = await fetchResource(collectionItem.id);
-                    await process(nestedJson.data, processedCollections);
-                }
-            }
-        }
+      }
     }
-    await process(jsonData, new Set());
-    return { firstManifest, manifestUrls, total: manifestUrls.length };
+    if (!foundManifests) {
+      for (const collectionItem of parser.iterateCollectionCollection()) {
+        if (collectionItem.items) {
+          await process(collectionItem, processedCollections);
+        } else {
+          const nestedJson = await fetchResource(collectionItem.id);
+          await process(nestedJson.data, processedCollections);
+        }
+      }
+    }
+  }
+  await process(jsonData, new Set());
+  return { firstManifest, manifestUrls, total: manifestUrls.length };
 }
-
 
 async function fetchManifest(manifestId: string): Promise<IIIFManifest> {
-    if (manifestCache.has(manifestId)) {
-        return manifestCache.get(manifestId)!;
-    }
+  if (manifestCache.has(manifestId)) {
+    return manifestCache.get(manifestId)!;
+  }
 
-    const manifestResource = await fetchResource(manifestId);
-    const parsedManifest = await parseManifest(manifestResource.data);
-    manifestCache.set(manifestId, parsedManifest);
-    return parsedManifest;
+  const manifestResource = await fetchResource(manifestId);
+  const parsedManifest = await parseManifest(manifestResource.data);
+  manifestCache.set(manifestId, parsedManifest);
+  return parsedManifest;
 }
 
-export async function constructManifests(url: string): Promise<{ firstManifest: IIIFManifest | null, manifestUrls: string[], total: number }> {
-    const resource = await fetchResource(url);
+export async function constructManifests(
+  url: string,
+): Promise<{
+  firstManifest: IIIFManifest | null;
+  manifestUrls: string[];
+  total: number;
+}> {
+  const resource = await fetchResource(url);
 
-    if (resource.type === "Manifest") {
-        const parsedManifest = await parseManifest(resource.data);
-        const manifestUrls = [url]; // Add the single manifest URL to the array
-        return { firstManifest: parsedManifest, manifestUrls, total: 1 };
-    } else if (resource.type === "Collection") {
-        const { firstManifest, manifestUrls, total } = await parseCollection(resource.data);
+  if (resource.type === 'Manifest') {
+    const parsedManifest = await parseManifest(resource.data);
+    const manifestUrls = [url]; // Add the single manifest URL to the array
+    return { firstManifest: parsedManifest, manifestUrls, total: 1 };
+  } else if (resource.type === 'Collection') {
+    const { firstManifest, manifestUrls, total } = await parseCollection(
+      resource.data,
+    );
 
-        if (manifestUrls.length === 0) {
-            return { firstManifest: null, manifestUrls: [], total: 0 };
-        }
-
-        if (!firstManifest && manifestUrls.length > 0) {
-            const firstFetchedManifest = await fetchManifest(manifestUrls[0]);
-            return { firstManifest: firstFetchedManifest, manifestUrls, total };
-        }
-
-        return { firstManifest, manifestUrls, total };
+    if (manifestUrls.length === 0) {
+      return { firstManifest: null, manifestUrls: [], total: 0 };
     }
 
-    throw new TamerlaneParseError("Unknown IIIF resource type");
-}
+    if (!firstManifest && manifestUrls.length > 0) {
+      const firstFetchedManifest = await fetchManifest(manifestUrls[0]);
+      return { firstManifest: firstFetchedManifest, manifestUrls, total };
+    }
 
+    return { firstManifest, manifestUrls, total };
+  }
+
+  throw new TamerlaneParseError('Unknown IIIF resource type');
+}
 
 // constructManifests("https://iiif.io/api/cookbook/recipe/0266-full-canvas-annotation/manifest.json")
 //     .then(manifest => console.log(JSON.stringify(manifest, null, 2))) // Pretty-print JSON
 //     .catch(console.error);
 
-  constructManifests("https://iiif.wellcomecollection.org/presentation/b19974760_1_0007")
-  .then(manifest => console.log(JSON.stringify(manifest, null, 2))) // Pretty-print JSON
-  .catch(console.error);  
+constructManifests(
+  'https://iiif.wellcomecollection.org/presentation/b19974760_1_0007',
+)
+  .then((manifest) => console.log(JSON.stringify(manifest, null, 2))) // Pretty-print JSON
+  .catch(console.error);
